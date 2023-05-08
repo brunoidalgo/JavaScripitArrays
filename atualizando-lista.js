30dd16d1942928a1b621a0f08221cdd5ec8344d1
brunoidalgo <idalgob99@gmail.com>
2023-05-07 21:48:47 -0300

    Mais sobre Arrays

--- atualizando-lista.js
+++ atualizando-lista.js
@@ -1,7 +1,7 @@
 
 const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
 
-nomes.splice(1, 2, "Rodrigo");
-// nomes.push("Rodrigo");
+nomes.splice(1, 2, "Rodrigo"); // Remove os dados das posições 1 e 2 e substitui pelo 3 dado informado que nesse caso é "Rodrigo"
+// nomes.push("Rodrigo"); // Esse método adicionaria o nome "Rodrigo" em ultimo lugar na lista sem substituir ninguém , então somaria.
 
 console.log(nomes);
---
+++ juntando-sala.js
@@ -0,0 +1,6 @@
+const salaJs = ["Sara", "Bruno", "Jéssica "];
+const salaPy = ["José", "Maria", "Paulo "];
+
+const palestra = salaJs + salaPy
+
+console.log(palestra);
